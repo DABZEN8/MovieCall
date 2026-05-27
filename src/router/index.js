6673e4df0b5ff687e1386355c4a0d46d3e8a7a04
Bibbe <180316166+Bibbee@users.noreply.github.com>
2026-05-28 00:01:54 +0200

    added routes and main navigation layout
    Co-Authored-By: ZONEH <204723995+DABZEN8@users.noreply.github.com>
    Co-Authored-By: Goran <184881182+GorSla14@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from "@/views/homeView.vue"
+import MyListView from "@/views/MyListView.vue"
+import WeekView from "@/views/WeekView.vue"
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/my-list",
+    name: "my-list",
+    component: MyListView,
+  },
+  {
+    path: "/week",
+    name: "week",
+    component: WeekView,
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+//Här ska search bar och results in
+</script>
+//placeholder
+<template>
+    <h2>Search for movies</h2>
+    <p>Search form goes here.</p>
+</template>
